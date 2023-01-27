7d52483dd080e92c3d595aa3a58cdffeb936c6e2
Max <max@privatevoid.net>
2023-01-27 03:03:33 +0100

    basic hyprland setup

--- nixos/hosts/default/gui.nix
+++ nixos/hosts/default/gui.nix
@@ -5,8 +5,14 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd TODO";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
       };
     };
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    nvidiaPatches = true;
   };
 }
