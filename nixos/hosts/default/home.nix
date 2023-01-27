79399ae9aaa2427154015991446ae58e05236eff
Max <max@privatevoid.net>
2023-01-27 23:43:10 +0100

    port hyprland autostart

--- nixos/hosts/default/home.nix
+++ nixos/hosts/default/home.nix
@@ -1,10 +1,49 @@
-{ osConfig, ... }:
+{ osConfig, pkgs, ... }:
+
+let
+  wallpaper = pkgs.callPackage ./wallpapers/city.nix { };
+
+  autostart = pkgs.writeShellScript "hyprland-session-autostart" ''
+    hyprctl setcursor Bibata-Modern-Ice 24
+  '';
+in
 
 {
   home.stateVersion = "23.05";
+
+  home.packages = with pkgs; [
+    bibata-cursors
+  ];
+
   wayland.windowManager.hyprland = {
     enable = true;
     inherit (osConfig.programs.hyprland) package;
-    extraConfig = builtins.readFile ../../../config/hypr/hyprland.conf;
+    extraConfig = ''
+      exec-once = ${autostart}
+    '' + builtins.readFile ../../../config/hypr/hyprland.conf;
+  };
+
+  services.dunst = {
+    enable = true;
+    configFile = ../../../config/dunst/dunstrc;
+  };
+
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+  };
+
+  systemd.user.services.swaybg = {
+    Unit = {
+      Description = "swaybg wallpaper daemon";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.swaybg}/bin/swaybg -i ${wallpaper} -m fill";
+    };
+
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
